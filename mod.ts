d8b5ac849efdb273bec66e7df1bfb278fc034baa
glebbash <glebbash@gmail.com>
2020-06-01 17:54:45 +0300

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,5 +1,3 @@
-export {
-    RegExpParser,
-    BaseRegExpVisitor,
-    VERSION,
-} from "https://cdn.pika.dev/regexp-to-ast";
+import * as mod from 'https://cdn.pika.dev/regexp-to-ast@^0.5.0';
+const { RegExpParser, BaseRegExpVisitor, VERSION } = mod.default;
+export { RegExpParser, BaseRegExpVisitor, VERSION };
